28dca6d73b0695f88d677992c47d46caa6354c64
whw188 <wangwei.cs@bytedance.com>
2024-06-18 22:31:03 +0800

    callbundle 余额检查，更新合约

--- internal/web3ext/web3ext.go
+++ internal/web3ext/web3ext.go
@@ -655,6 +655,11 @@ web3._extend({
 			params: 1,
 		}),
 		new web3._extend.Method({
+			name: 'callBundleCheckBalance',
+			call: 'eth_callBundleCheckBalance',
+			params: 1,
+		}),
+		new web3._extend.Method({
 			name: 'multicall',
 			call: 'eth_multicall',
 			params: 2,
